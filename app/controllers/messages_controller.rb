059e049cba24f98e9fe3b219223ee2dc19af547c
Benjamin Hsieh <bhsiehh@gmail.com>
2014-09-02 16:56:00 -0700

    Restrict access to limited set of email addresses

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,13 @@ class MessagesController < ApplicationController
   before_filter :authorize, only: [:index]
 
   def index
-    @messages = Message.all.order('timestamp asc')
+    if current_user.email.in?(ENV['VALID_EMAILS'].split(','))
+      @messages = Message.all.order('timestamp asc')
+    else
+      sign_out
+      flash[:error] = 'Unauthorized.'
+      redirect_to root_path
+    end
   end
 
   def create
