1eb4fa596e3bd5f2e970e91e2b100557099bf8fd
Benjamin Hsieh <bhsieh@twitch.tv>
2015-06-03 16:25:54 -0700

    Refactor parsing logic

--- main.go
+++ main.go
@@ -8,27 +8,15 @@ import (
 
   // Webserver dependencies
   "net/http"
-  "encoding/json"
   "github.com/gorilla/mux"
 
   // Database dependencies
   _ "github.com/lib/pq"
   "github.com/jmoiron/sqlx"
+  "io"
+  "io/ioutil"
+  "net/url"
 )
-
-type Message struct {
-  Token string `json:"token"`
-  TeamID string `json:"team_id"`
-  TeamDomain string `json:"team_domain"`
-  ChannelID string `json:"channel_id"`
-  ChannelName string `json:"channel_name"`
-  Timestamp string `json:"timestamp"`
-  UserID string `json:"user_id"`
-  UserName string `json:"user_name"`
-  Text string `json:"text"`
-  TriggerWord string `json:"trigger_word"`
-  ServiceID string `json:"service_id"`
-}
 
 func main() {
   r := mux.NewRouter()
@@ -43,17 +31,19 @@ func PingHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func MessageHandler(w http.ResponseWriter, r *http.Request) {
-  decoder := json.NewDecoder(r.Body)
+  body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+  if err != nil {
+    panic(err)
+  }
 
-  var message Message
-  err := decoder.Decode(&message)
+  parsed, err := url.ParseQuery(string(body))
   if err != nil {
     panic(err)
   }
 
   w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 
-  if message.Token != os.Getenv("TOKEN") {
+  if parsed.Get("token") != os.Getenv("TOKEN") {
     w.WriteHeader(http.StatusBadRequest)
   } else {
     db, err := sqlx.Connect("postgres", os.Getenv("DB_INFO"))
@@ -62,9 +52,8 @@ func MessageHandler(w http.ResponseWriter, r *http.Request) {
     }
     db.MustExec(
       `INSERT INTO messages(token,team_id,team_domain,channel_id,channel_name,timestamp,user_id,user_name,text,trigger_word,service_id,created_at,updated_at) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13);`,
-      message.Token, message.TeamID, message.TeamDomain, message.ChannelID, message.ChannelName, message.Timestamp, message.UserID, message.UserName, message.Text, message.TriggerWord, message.ServiceID, time.Now(), time.Now())
+      parsed.Get("Token"), parsed.Get("TeamID"), parsed.Get("TeamDomain"), parsed.Get("ChannelID"), parsed.Get("ChannelName"), parsed.Get("Timestamp"), parsed.Get("UserID"), parsed.Get("UserName"), parsed.Get("Text"), parsed.Get("TriggerWord"), parsed.Get("ServiceID"), time.Now(), time.Now())
     defer db.Close()
-
     w.WriteHeader(http.StatusOK)
   }
 }
